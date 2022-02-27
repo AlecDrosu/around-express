fc83bf4f47a7535241a2efcfbce373fc8576df59
Alec <drosualec@gmail.com>
2022-02-26 22:39:31 -0500

    Project complete for Review 1

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-// create an express server and configure it to run on port 3000
-// by running the following command:
-// npm run start
-
 const express = require("express");
 
 const app = express();
-// const path = require("path");
+
 const usersRouter = require("./routes/users");
 const cardsRouter = require("./routes/cards");
 
 const { PORT = 3000 } = process.env;
 
-// app.use(express.static(path.join(__dirname, "public")));
 app.use("/", usersRouter);
 app.use("/", cardsRouter);
 
